8b6d8c73cc1192c00da1b0458222534934f0a029
sebasch <sebaxtz@gmail.com>
2020-06-11 16:46:16 -0400

    asdasd

--- webhook.php
+++ webhook.php
@@ -4,7 +4,20 @@ $post=implode($_POST);
 
 error_log(print_r($post,true));
 
+error_log(print_r($_POST,true));
+
+// error_log($_POST);
 
 print_r($post,true);
+
+
+foreach($_POST as $item) {
+
+  error_log($item);
+
+  echo($item);
+  print($item);
+}
+
 
 ?>
