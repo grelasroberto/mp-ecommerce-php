8bc4d29d67e8bdcab83191366df3fc3bafa46dbc
sebasch <sebaxtz@gmail.com>
2020-06-11 15:42:23 -0400

    asda

--- pay.php
+++ pay.php
@@ -3,7 +3,7 @@
 
 require __DIR__ .  '/vendor/autoload.php';
 
-echo "lalala";
+// echo "lalala";
 
 MercadoPago\SDK::setAccessToken('TEST-1730556772600131-042800-fd80f3255535e0a46c3091d83290484d-373768843');
 
@@ -12,7 +12,7 @@ MercadoPago\SDK::setAccessToken('TEST-1730556772600131-042800-fd80f3255535e0a46c
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
 
-// Crea un ítem en la preferencia
+// Crea un ítem en la preferenciag
 $item = new MercadoPago\Item();
 $item->title =  $_POST['title'];
 $item->description ='Dispositivo móvil de Tienda e-commerce';
@@ -22,12 +22,36 @@ $item->unit_price =   $_POST['price'];
 $preference->items = array($item);
 $preference->external_reference="sebasch@maximussoft.com";
 
+$payer = new MercadoPago\Payer();
+$payer->name = "Lalo";
+$payer->surname = "Landa";
 $payer->address = array(
   "street_name" => "False",
   "street_number" => 123,
   "zip_code" => "1111"
 );
 
+$payer->phone->area_code="11";
+$payer->phone->number="22223333";
+
+
+$preference->back_urls = array(
+    "success" => "https://grelasroberto-mp-commerce-php.herokuapp.com/payok.php",
+    "failure" => "https://grelasroberto-mp-commerce-php.herokuapp.com/failed.php",
+    "pending" => "https://grelasroberto-mp-commerce-php.herokuapp.com/paypending.php"
+);
+$preference->auto_return = "approved";
+
+
+$preference->payment_methods = array(
+  "excluded_payment_methods" => array(
+    array("id" => "amex")
+  ),
+  "excluded_payment_types" => array(
+    array("id" => "atm")
+  ),
+  "installments" => 6
+);
 $preference->payer = $payer;
 $preference->save();
 
