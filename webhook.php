c67ee930157a2827a8beb7c13a0efa2a1495aa0b
sebasch <sebaxtz@gmail.com>
2020-06-11 17:14:20 -0400

    asdfsad

--- webhook.php
+++ webhook.php
@@ -18,30 +18,12 @@ echo "ITEMMMM $item" ;
   print($item);
 }
 
-require __DIR__ .  '/vendor/autoload.php';
+// require __DIR__ .  '/vendor/autoload.php';
 
 // echo "lalala";
 
-MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+// MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
-$ss=MercadoPago\Payment.find_by_id($_POST["id"]);
-error_log("FUCCCCCCCCCCCCCCCCC ss");
-
-   switch($_POST["type"]) {
-       case "payment":
-           $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-           break;
-       case "plan":
-           $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
-           break;
-       case "subscription":
-           $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
-           break;
-       case "invoice":
-           $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
-           break;
-   }
-
- error_log($payment);
+ // error_log($payment);
 
 ?>
